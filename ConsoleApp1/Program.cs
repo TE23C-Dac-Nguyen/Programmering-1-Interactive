d9806ff7b3aca6e7e167febac6b714455eca2d74
dac nguyen <dac.nguyen@elev.ga.ntig.se>
2024-09-13 13:41:52 +0200

    Interaktiv berättelse

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,43 @@
-﻿Console.WriteLine ("användarernamn: ");
+﻿Console.WriteLine ("Name: ");
 String name = Console.ReadLine();
 
 if (name == "Bao")
 {
-    Console.WriteLine("Välkommen Bao!");
+    Console.WriteLine("Welcome!");
     
 }
-else
-{
-    Console.WriteLine("nuh uh");
+Console.WriteLine ("Age: "); 
+String age = Console.ReadLine();
+
+int x = 18;
+
+
+if (x > 18 && x == 18) {
+
+Console.WriteLine("You are legal, Access accepted!");
+}
+else     {
+  Console.WriteLine("You are not legal, Access denied!");
+
 }
 
+Console.WriteLine ("Choose a random number between 1 to 6");
+String randomNumber = Console.ReadLine();
+
+Random random = new Random();
+int dice = random.Next(1, 7);
+
+Console.WriteLine("TThe Dice result is: ");
+Console.WriteLine(dice);
+
+if (dice == Convert.ToInt32(randomNumber)) 
+
+ 
+ {
+    Console.WriteLine("Congratulations! You guessed correctly!");
+ }
+ else     {
+  Console.WriteLine("Sorry, you guessed incorrectly!");
+
+}
 Console.ReadLine();
